6ff9d41dbd197cfd4153741d710ae5bf589f2a61
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-02-16 16:43:31 -0500

    Update post-create.sh

--- .devcontainer /post-create.sh
+++ .devcontainer /post-create.sh
@@ -1,5 +1,3 @@
-printf "Start of Script File\n" > /workspaces/BigLog.txt
-
 # Access the GITHUB_REPOSITORY environment variable
 repo=$GITHUB_REPOSITORY
 
@@ -7,23 +5,23 @@ repo=$GITHUB_REPOSITORY
 IFS='/' read -ra ADDR <<< "$repo"
 owner=${ADDR[0]}
 repository=${ADDR[1]}
+printf "Start of Script File\n" > /workspaces/$repository/BigLog.txt
+printf "Owner: $owner and Repository: $repository\n" > /workspaces/$repository/BigLog.txt
 
-printf "Owner: $owner and Repository: $repository\n" > /workspaces/BigLog.txt
 
-
-printf "CD command\n" >> /workspaces/BigLog.txt
+printf "CD command\n" >> /workspaces/$repository/BigLog.txt
 cd /usr/share
 
 mv dotnet dotnet-8.0.101
 mkdir dotnet
 
-printf "curl command\n" >> /workspaces/BigLog.txt
+printf "curl command\n" >> /workspaces/$repository/BigLog.txt
 curl -Lo dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/85bcc525-4e9c-471e-9c1d-96259aa1a315/930833ef34f66fe9ee2643b0ba21621a/dotnet-sdk-8.0.201-linux-x64.tar.gz
 
-printf "tar command\n" >> /workspaces/BigLog.txt
+printf "tar command\n" >> /workspaces/$repository/BigLog.txt
 tar -C dotnet -xf dotnet.tar.gz
 
-printf "remove file\n" >> /workspaces/BigLog.txt
+printf "remove file\n" >> /workspaces/$repository/BigLog.txt
 rm dotnet.tar.gz
 
-printf "End of Script File" >> /workspaces/BigLog.txt
+printf "End of Script File" >> /workspaces/$repository/BigLog.txt
